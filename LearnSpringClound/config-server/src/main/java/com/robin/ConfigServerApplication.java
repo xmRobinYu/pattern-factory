1e9f10d83033314af444b1065763cf79747c5e56
10359639@qq.com <yqp871104>
2019-05-07 16:24:14 +0800

    新增配置服务器

---
+++ LearnSpringClound/config-server/src/main/java/com/robin/ConfigServerApplication.java
@@ -0,0 +1,26 @@
+package com.robin;
+
+
+import cn.hutool.core.util.NetUtil;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        int port = 8030;
+        if(!NetUtil.isUsableLocalPort(port)) {
+            System.err.printf("端口%d被占用了，无法启动%n", port );
+            System.exit(1);
+        }
+        new SpringApplicationBuilder(ConfigServerApplication.class).properties("server.port=" + port).run(args);
+
+    }
+}
